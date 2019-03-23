ad275eaaecc7ece85e1624494869eb1977378c89
Thusitha <thusitha@Thusithas-MacBook-Air.local>
2019-03-23 17:03:30 +0530

    thread locking

--- src/main/java/com/niroshan/threads/Bank.java
+++ src/main/java/com/niroshan/threads/Bank.java
@@ -1,6 +1,24 @@
 package com.niroshan.threads;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/*
+* ReentrantLock has been introduced to gain a lock before go to the critical section,
+* in here transferring the money
+*
+* Condition is used make the thrad wait if the minimum condition does not meet
+* Once the above condition is fulfilled by another thread that thread calls notifyAll
+* after notifying it waiting threads start to work again from the beginning
+* 
+*
+* */
+
 public class Bank {
+
+    Lock bankLock ;
+    Condition suficientFunds;
 
 
     private final double[] accounts;
@@ -11,25 +29,46 @@ public class Bank {
         for(int i=0 ; i < n; i++){
             accounts[i] = initialBalance;
         }
+
+        bankLock = new ReentrantLock();
+        suficientFunds = bankLock.newCondition();
+
     }
 
-    public void transfar(int from, int to,double amount){
 
-        if(accounts[from]< amount) return;
-        System.out.println(Thread.currentThread());
-        accounts[from] -= amount;
-        accounts[to] += amount;
-        System.out.println("From: " +from+"\tTo: "+to+"\tAmount: "+amount);
-        System.out.println(getTotalBalance());
+    public void transfar(int from, int to,double amount)throws InterruptedException{
+            bankLock.lock();
+            try {
+
+                if (accounts[from] < amount)
+                    suficientFunds.await();
+                System.out.println(Thread.currentThread());
+                accounts[from] -= amount;
+                System.out.printf("From: " + from + "\tTo: " + to + "\tAmount: " + amount + " total");
+                accounts[to] += amount;
+                System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+                suficientFunds.signalAll();
+            }finally {
+                bankLock.unlock();
+            }
     }
 
     public double getTotalBalance(){
-        double sum = 0;
 
-        for(double amt : accounts){
-            sum+=amt;
-        }
-        return sum;
+            bankLock.lock();
+            try {
+                double sum = 0;
+
+                for (double amt : accounts) {
+                    sum += amt;
+                }
+
+                return sum;
+            }
+            finally {
+                bankLock.unlock();
+            }
+
     }
 
     public int size(){
--- src/main/java/com/niroshan/threads/TransferRunnable.java
+++ src/main/java/com/niroshan/threads/TransferRunnable.java
@@ -15,7 +15,7 @@ public class TransferRunnable implements Runnable {
     private Bank bank;
     private int from;
     private double maxAmount;
-    private int DELAY = 10;
+    private int DELAY = 5;
 
     public TransferRunnable(Bank b,int from,double maxAmount){
         this.bank = b;
@@ -32,6 +32,7 @@ public class TransferRunnable implements Runnable {
             while (true){
                 int toAccount = (int) (bank.size()*Math.random());
                 double amount =maxAmount * Math.random();
+
                 bank.transfar(from,toAccount,amount);
                 Thread.sleep(DELAY);
             }
